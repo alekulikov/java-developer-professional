94ed8ae995cc878d1552beee78037c65391c3fcb
alekulikov <99183267+alekulikov@users.noreply.github.com>
2023-11-13 14:43:18 +0300

    hw01-gradle (#1)
    * info -> README.md
    
    * add .gitignore
    
    * root project gradle init
    
    * add module hw01-gradle
    
    ---------
    
    Co-authored-by: alekulikov <aleksandr.a.kulikov@mvideo.ru>

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    idea
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+    id("name.remal.sonarlint") apply false
+    id("com.diffplug.spotless") apply false
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    val testcontainersBom: String by project
+    val protobufBom: String by project
+    val guava: String by project
+
+    plugins.apply("io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom(SpringBootPlugin.BOM_COORDINATES)
+                mavenBom("org.testcontainers:testcontainers-bom:$testcontainersBom")
+                mavenBom("com.google.protobuf:protobuf-bom:$protobufBom")
+            }
+            dependency("com.google.guava:guava:$guava")
+        }
+    }
+}
+
+subprojects {
+    plugins.apply("java")
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+
+    plugins.apply("name.remal.sonarlint")
+    plugins.apply("com.diffplug.spotless")
+    extensions.configure<SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.38.0")
+        }
+    }
+}
+
+tasks {
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                    .managedVersions
+                    .toSortedMap()
+                    .map { "${it.key}:${it.value}" }
+                    .forEach(::println)
+        }
+    }
+}
