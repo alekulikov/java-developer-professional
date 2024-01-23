167d7af4679b95d4fa36e75e69b81507da50dcfa
alekulikov <99183267+alekulikov@users.noreply.github.com>
2024-01-23 11:49:20 +0300

    hw08-io (#9)
    Co-authored-by: alekulikov <aleksandr.a.kulikov@mvideo.ru>

--- hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,27 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.IOException;
 import java.util.List;
 import ru.otus.model.Measurement;
 
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private final String fileName;
+
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public List<Measurement> load() {
         // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        try (var inputStream = ResourcesFileLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            var objectMapper = JsonMapper.builder().build();
+            return objectMapper.readValue(inputStream, new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
